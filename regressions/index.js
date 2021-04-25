3e31ecb92570ea88af996c6ec3fa3a0dde3e115e
Aqib <aqib.ahamad21@gmail.com>
2021-04-25 21:31:18 +0300

    vectorizing linear regression

--- regressions/index.js
+++ regressions/index.js
@@ -2,19 +2,28 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
     shuffle: true,
     splitTest: 50,
-    dataColumns: ['horsepower'],
+    dataColumns: ['horsepower', 'weight', 'displacement'],
     labelColumns: ['mpg']
 });
 
 const regression = new LinearRegression(features, labels, {
-    learningRate: 0.0001,
+    learningRate: 0.1,
     iterations: 100
 });
 
 regression.train();
 
-console.log('Updated M:', regression.m, 'Updated B:', regression.b);
+const r2 = regression.test(testFeatures, testLabels);
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error'
+});
+
+console.log('R2', r2)
